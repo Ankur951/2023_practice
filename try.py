9ea403e43c1c74632402bf5fa8d4cf69e4d85f30
Ankur951 <128561484+Ankur951@users.noreply.github.com>
2023-03-24 10:29:29 +0530

    Update try.py

--- try.py
+++ try.py
@@ -1,2 +1,13 @@
-print("my first git commit")
-print("my first git commit")
+"""
+dinamic way to creat a table for any number
+"""
+
+
+def print_table(table_for, loop):
+    for item in range(loop):
+        print("{0} * {1} = {2}".format(table_for, (item + 1), table_for * (item + 1)))
+
+
+table_for = 15
+loop = 20
+print_table(table_for, loop)
