a20d7f50dc1050047939eccea71eee0812076c0e
Ankur951 <128561484+Ankur951@users.noreply.github.com>
2023-04-10 10:17:31 +0530

    split_and_join
    i split the given string and join "-"

---
+++ split_and_join.py
@@ -0,0 +1,17 @@
+string = (input("plies enter a string="))
+
+
+def split_a_join(line):
+    line = line.split(" ")
+    line = "-".join(line)
+    print(line)
+
+
+def split_and_join(line):
+    line = line.split(" ")
+    line = "-".join(line)
+    print(line)
+
+
+split_and_join("this is a string")
+split_a_join(string)
