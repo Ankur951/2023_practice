d7c047b846f291d26643b336d957e7dbe3cf5025
Ankur951 <128561484+Ankur951@users.noreply.github.com>
2023-04-12 14:59:11 +0530

    Update Complete.py

--- Complete.py
+++ Complete.py
@@ -4,7 +4,7 @@ Name = input("Plies inter name=")
 def Complete(string):
     string = string.split(" ")
     for i in range(len(string)):
-        if string[i][0].islower():
+        if string[i].islower() and string[i] != "":
             string[i] = string[i].replace(string[i][0], string[i][0].upper(), 1)
 
     string = " ".join(string)
